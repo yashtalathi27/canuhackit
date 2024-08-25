8348ade1257dbbb3b7fb810eda3ee5551f3b177a
AnupamSingh2004 <sanupam2004@gmail.com>
2024-08-25 18:08:20 +0530

    Slices

--- gamecommunity/src/App.jsx
+++ gamecommunity/src/App.jsx
@@ -1,6 +1,4 @@
 import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from '../src/components/header/header';
 import Home from './components/Home';
--- gamecommunity/src/store/index.js
+++ gamecommunity/src/store/index.js
@@ -1,7 +1,7 @@
 import {create} from "zustand";
-import {createAuthSlice} from "@/store/slices/auth-slice.js";
+import {createAuthSlice} from "./slices/auth-slice.js";
 
 
 export const useAppStore = create()((...a) => ({
     ...createAuthSlice(...a)
-}))
+})) 
